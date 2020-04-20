72085f7e4a7ba22f1ef3f49543b8ecd097d86d74
조덕행 <62981065+DeokCho@users.noreply.github.com>
2020-04-20 19:23:52 +0900

    2020.04.20

--- java/com/occamsrazor/web/member/MemberController.java
+++ java/com/occamsrazor/web/member/MemberController.java
@@ -1,7 +1,19 @@
 package com.occamsrazor.web.member;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MemberController {
-	public static void main(String[] args) {
-		
+	public MemberService memberService;
+	
+	
+	@PostMapping("/join")
+	public Member add(@RequestBody Member member) {
+		System.out.println(member.toString());
+		memberService = new MemberServiceImpl();
+		memberService.add(member);
+		return member;
 	}
 }
